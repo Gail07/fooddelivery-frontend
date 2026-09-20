3d8bb79ac694d2518688804cd65524deead06b76
Kacou Abigail <kacouabigail0@gmail.com>
2026-09-20 22:46:42 +0000

    feat: add food list fetching and token persistence

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,37 +1,52 @@
 import { createContext, useEffect, useState } from 'react';
-import {food_list} from '../assets/assets'
+
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const [cartItems,setCartItems] = useState({});
-    const url = "http://localhost:4000"
+    const [cartItems, setCartItems] = useState({});
+    const url = "http://localhost:4000";
     const [token, setToken] = useState("");
+    const [food_list, setFoodList] = useState([])
 
     const addToCart = (itemId) => {
         if (!cartItems[itemId]) {
-            setCartItems((prev)=>({...prev,[itemId]:1}))
+            setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
         }
         else {
-            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+            setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
         }
     }
 
     const removeFromCart = (itemId) => {
-        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
     }
 
-   const getTotalCartAmount = () => {
-    let totalAmount = 0;
-    for (const item in cartItems) {
-        if (cartItems[item] > 0) {
-            let itemInfo = food_list.find((product) => product._id === item);
-            totalAmount += itemInfo.price * cartItems[item];
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = food_list.find((product) => product._id === item);
+                totalAmount += itemInfo.price * cartItems[item];
+            }
         }
+        return totalAmount;
     }
-    return totalAmount;
-   }
+
+    const fetchFoodList = async () => {
+        setFoodList(response.data.data)
+    }
+
+    useEffect(() => {
+        async function loadData() {
+            await fetchFoodList();
+            if (localStorage.getItem("token")) {
+                setToken(localStorage.getItem("token"));
+            }
+            loadData();
+        }
+    }, [])
 
     const conextValue = {
         food_list,
